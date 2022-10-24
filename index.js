4107918f9b7cb37f48274aa5109d13df56dcecde
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-24 16:33:02 -0400

    Completed API

--- index.js
+++ index.js
@@ -3,25 +3,41 @@ const app = express();
 const arithmeticFunctions = require('./arithmeticFunctions');
 app.use(express.urlencoded({extended: false}));
 
-let 
 
 app.get('/', function(req, res){
     console.log(req.query);
-
-    res.send('Arithmetic Calculation');
-})
+    res.send('Arithmetic Calculator');
+});
 
 app.post('/', function(req, res){
-    // students.push(req.body);
-    // res.redirect('/');
-    const calculator = {
+    let data = {
         Operation: req.body.Operation, 
         Value_1: req.body.Value_1,
-        Value_2: req.body.Value_2
+        Value_2: req.body.Value_2,
     };
 
+    if(data.Operation == 'add'){
+        let Result = arithmeticFunctions.add(parseInt(data.Value_1), parseInt(data.Value_2));
+        res.send(`Operation: Addition, Value 1: ${data.Value_1}, Value 2: ${data.Value_2}, Result: ${Result}`);
+        console.log(`Operation: Addition, Value 1: ${data.Value_1}, Value 2: ${data.Value_2}, Result: ${Result}`);
+    };
 
-    console.log('Operation: req.body.Operation, Value 1: ${Value_1} Value 2: ${Value_2} ')
+    if(data.Operation == 'sub'){
+        let Result = arithmeticFunctions.sub(parseInt(data.Value_1), parseInt(data.Value_2));
+        res.send(`Operation: Subtraction, Value 1: ${data.Value_1}, Value 2: ${data.Value_2}, Result: ${Result}`);
+    };
+    
+    if(data.Operation == 'mul'){
+        let Result = arithmeticFunctions.multiply(parseInt(data.Value_1), parseInt(data.Value_2));
+        res.send(`Operation: Multiplication, Value 1: ${data.Value_1}, Value 2: ${data.Value_2}, Result: ${Result}`);
+    }; 
+    
+    if(data.Operation == 'div'){
+        let Result = arithmeticFunctions.divide(parseInt(data.Value_1), parseInt(data.Value_2));
+        res.send(`Operation: Division, Value 1: ${data.Value_1}, Value 2: ${data.Value_2}, Result: ${Result}`);
+    };  
+
+    res.redirect('/');
 });
 
 app.listen(3000, function(){
