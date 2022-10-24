880053e58b6d61f5bbe67c7da7d9fb7419c6ef93
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-24 01:12:44 -0400

    Setup server

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const app = express();
 const arithmeticFunctions = require('./arithmeticFunctions');
 app.use(express.urlencoded({extended: false}));
 
+let 
+
+app.get('/', function(req, res){
+    console.log(req.query);
+
+    res.send('Arithmetic Calculation');
+})
+
+app.post('/', function(req, res){
+    // students.push(req.body);
+    // res.redirect('/');
+    const calculator = {
+        Operation: req.body.Operation, 
+        Value_1: req.body.Value_1,
+        Value_2: req.body.Value_2
+    };
 
 
+    console.log('Operation: req.body.Operation, Value 1: ${Value_1} Value 2: ${Value_2} ')
+});
 
 app.listen(3000, function(){
     console.log('Server running on port 3000...')
